51f59c1360521d361ec0481cb7297fcfa1c3230e
AsepGinanjar <136257590+AsepGinanjar@users.noreply.github.com>
2025-04-16 10:43:49 +0700

    first

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors"); // Tambahkan ini
+const app = express();
+const port = 8000;
+
+// Izinkan permintaan dari semua origin (atau bisa dibatasi ke origin tertentu)
+app.use(cors()); // Middleware CORS
+
+// Endpoint API
+app.get("/api/data", (req, res) => {
+  res.json({ message: "Halo dari Backend Node.js!" });
+});
+
+// Jalankan server di semua IP agar bisa diakses dari jaringan lokal
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server backend berjalan di port ${port}`);
+});
